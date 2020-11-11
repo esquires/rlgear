7bf348865c1cb6c024acfdeffb90f36ab5f4e4fd
David Pendleton // Research Engineer I // GTRI-ICL-SEAD-HDAB-ATL <dpendleton6-gtri@ice180.icehammer.gtri.org>
2020-11-11 13:32:27 -0500

    added function and flag within utils.py to correct the offset index previously seen in plot_progress, the process_pbt flag in the function is defaulted to False

--- rlgear/utils.py
+++ rlgear/utils.py
@@ -262,6 +262,21 @@ def shorten_dfs(_dfs: Sequence[pd.DataFrame], max_step: int = None) -> None:
         _dfs[i] = _df[_df.index <= max_step]  # type: ignore
 
 
+def preprocess_pbt_df(df: pd.DataFrame, x_tag: str) -> pd.DataFrame:
+    tag_diff = '{}_diff'.format(x_tag)
+    df[tag_diff] = df[x_tag].diff()
+    neg_diff = df.loc[df[tag_diff] < 0].index.to_list()
+    if len(neg_diff) == 0:
+        return df
+    else:
+        for _, val in enumerate(neg_diff):
+            start = val
+            end = len(df)
+            df[x_tag][start:end] += df[tag_diff][start-2] +\
+                abs(df[tag_diff][start])
+    return df
+
+
 def plot_percentiles(
         ax: plt.axis, df: pd.DataFrame, percentiles: Tuple[float, float],
         alpha: float) -> None:
@@ -278,11 +293,13 @@ def plot_percentiles(
 
 
 # pylint: disable=too-many-locals
+# pylint: disable=too-many-branches
 def plot_progress(
         ax: plt.axis,
         base_dirs: Iterable[StrOrPath],
         tag: str,
         x_tag: str = 'timesteps_total',
+        preprocess_pbt: bool = False,
         names: Optional[Iterable[str]] = None,
         only_complete_data: bool = False,
         show_same_num_timesteps: bool = False,
@@ -310,6 +327,8 @@ def plot_progress(
 
         for i, df in enumerate(dfs):
             try:
+                if preprocess_pbt:
+                    df = preprocess_pbt_df(df, x_tag)
                 dfs[i] = df[[x_tag, tag]].set_index(x_tag)
             except KeyError as e:
                 print(f'available keys are {df.columns}')

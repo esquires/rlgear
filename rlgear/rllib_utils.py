3e93972f2830e0a1949f91c9910e07fb91a60a61
Eric Squires <eric.squires@gtri.gatech.edu>
2020-11-21 14:08:45 -0500

    remove continue in meta logger (#31)

--- rlgear/rllib_utils.py
+++ rlgear/rllib_utils.py
@@ -1,9 +1,7 @@
 import argparse
 import re
 import os
-import sys
 import string
-import json
 import random
 import time
 from pathlib import Path
@@ -16,7 +14,7 @@ from ray.rllib.evaluation import MultiAgentEpisode
 from ray.rllib.agents.callbacks import DefaultCallbacks
 
 from .utils import MetaWriter, get_inputs, parse_inputs, get_log_dir, \
-    StrOrPath, dict_str2num, import_class, dict_import_class
+    StrOrPath, dict_str2num, import_class
 
 
 def make_rllib_metadata_logger(meta_data_writer: MetaWriter) \
@@ -25,42 +23,9 @@ def make_rllib_metadata_logger(meta_data_writer: MetaWriter) \
         def _init(self) -> None:
             self.meta_data_writer = meta_data_writer
             meta_data_writer.write(self.logdir)
-            self.saved_continue_script = False
 
         def on_result(self, _: dict) -> None:
-            if self.saved_continue_script:
-                return
-
-            self.saved_continue_script = True
-            # we log everything in the _init function but need to override
-            # on_result since it is a virtual method
-            log_path = Path(self.logdir).parent
-            json_fnames = log_path.glob('*.json')
-            json_fname = None
-            for fname in json_fnames:
-                with open(fname, 'r') as f:
-                    d = json.load(f)
-                if len(d['checkpoints']) == 1:
-                    # not supporting more than 1 checkpoint
-                    ckpt = d['checkpoints'][0]
-                    if ckpt['logdir'] == self.logdir:
-                        json_fname = fname
-                        break
-
-            if json_fname:
-                with open(log_path / 'continue.py', 'w') as f:
-                    f.write("\n".join([
-                        "import subprocess as sp",
-                        "from pathlib import Path",
-                        "restore_files = (Path(__file__).resolve() / 'meta').rglob('*_restore.py')",  # NOQA, pylint: disable=line-too-long
-                        "for f in restore_files:",
-                        "    sp.call(['python', str(f)])",
-                        f"cmd = ['{sys.executable}'] + {sys.argv}",
-                        r'cmd += ["--overrides", "{\"rllib\": {\"resume\": \"LOCAL\"}}"]',  # NOQA, pylint: disable=line-too-long
-                        "# make this checkpoint the most recent so it is",
-                        "# picked up by the rllib resume API",
-                        f"Path('{json_fname}').touch()",
-                        f"sp.call(cmd, cwd='{os.getcwd()}')"]))
+            pass
 
     return RllibLogMetaData
 
@@ -111,8 +76,6 @@ def make_basic_rllib_config(
 
     inputs = get_inputs(yaml_file, search_dirs)
     params = dict_str2num(parse_inputs(inputs))
-    params = dict_import_class(params)
-
     # override the non-rllib blocks
     if overrides is not None:
         params = ray.tune.utils.merge_dicts(params, overrides)
